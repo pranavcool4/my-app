dde8d19d86c5e0da7d11b8f44bf8b4d89e034bdc
pranavcool4 <pranavvcool4@gmail.com>
2019-03-27 22:51:13 +0530

    Updates

---
+++ precache-manifest.103fb2a1afd3d4bac1ddae8912394bde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "556122d365928eb8cf35",
+    "url": "/my-app/static/js/main.5afa555b.chunk.js"
+  },
+  {
+    "revision": "999ce4effe36e72c2b8e",
+    "url": "/my-app/static/js/2.d2bdd2f2.chunk.js"
+  },
+  {
+    "revision": "556122d365928eb8cf35",
+    "url": "/my-app/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "999ce4effe36e72c2b8e",
+    "url": "/my-app/static/css/2.ab733e66.chunk.css"
+  },
+  {
+    "revision": "ceb30b5713980a8114ee13288b7071bf",
+    "url": "/my-app/index.html"
+  }
+];
